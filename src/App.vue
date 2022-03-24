b3f2b7e4b8b9526a55e1ae38b69b8a7066d57cfe
Michael Thiessen <michael.thiessen@vidyard.com>
2022-03-24 09:09:52 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-gradient-to-br from-mt-light-green to-mt-light-blue w-full h-full">
+
+  </div>
+</template>
+
+<style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mt': {
+          'extra-light-blue': '#a3c0f5',
+          'light-blue': '#2F80ED',
+          'blue': '#0060E3',
+          'dark-blue': '#032A5E',
+
+          'light-green': '#A9F3D8',
+          'green': '#00CDA3',
+          'dark-green': '#0C714C',
+
+          'coral': '#EE7E96',
+          'orange': '#F4B18B',
+          'yellow': '#F3D58F',
+          'purple': '#43407B',
+        },
+        'type': {
+          'dark': '#282C2E',
+          'light': '#7B7E80',
+        },
+      },
+      outline: {
+        blue: '4px solid #3b82f680',
+      },
+    },
+  },
+  plugins: [],
+}
